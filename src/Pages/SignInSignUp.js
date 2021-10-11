a70141911f512292efe2e3051ccffd502efdbc2e
Mary1-5 <marymansha15@gmail.com>
2021-10-11 15:40:34 +0100

    LoginSignUpPages

---
+++ src/Pages/SignInSignUp.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './SignInSignUp.css'
+
+class SignInSignUp extends React.Component {
+
+    render() {
+        return (
+            <div className="SignInSignUp">
+                <section>
+                    <div className="container" id="container">
+                        <div className="form-container sign-in-container">
+                            <form className="SIform" action="#">
+                                <header className="SIheading">Welcome Back</header>
+                                <h1>Sign in</h1>
+                                <span className="SIprompt">to use your account</span>
+                                <div className="inputbars">
+                                    <input className="SIfields" type="email" placeholder="Email" />
+                                    <input className="SIfields" type="password" placeholder="Password" />
+                                </div>
+                                <a href="#">Forgot your password?</a>
+                                <button className="signIn">Sign In</button>
+                            </form>
+                        </div>
+                        <div className="overlay-container">
+                            <div className="overlay">
+                                <div className="overlay-panel overlay-left">
+                                    <h1 className="Account_SU">Hello, Friend!</h1>
+                                    <h4>Don't have an account?</h4>
+                                    <p className="SUdesc">Enter your personal details and start journey with us</p>
+                                    <button className="signUp" id="signUp">Sign Up</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
+
+export default SignInSignUp
