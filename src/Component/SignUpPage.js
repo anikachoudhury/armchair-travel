5fe7c436840cc514bdc005269fe88d9bc78f7696
Anika Choudhury <anikachoudhury010@gmail.com>
2021-10-11 12:36:28 +0100

    maryiahs section

---
+++ src/Component/SignUpPage.js
@@ -0,0 +1,43 @@
+import React from "react";
+import './SignUpPage.css';
+
+class SignUpPage extends React.Component {
+
+    render() {
+        return (
+            <div className="SignUp">
+                <section>
+                    <div className="SignUpContainer">
+                        <h1 className="CreateAccount">Create Account</h1>
+                    <form className="SignUpForm" action="#">
+                        <div className="SignUpInput">
+                            <label className="Sign-UpDetails">
+                                Email
+                                <span className="SignUpcontainer">
+                                    <input type="email" name="email" id="email" placeholder="Enter your email..." />
+                                </span>
+                            </label>
+                            <label className="Sign-UpDetails">
+                                Username
+                                <span className="SignUpcontainer">
+                                    <input type="text" name="username" id="username" placeholder="Enter a username..." />
+                                </span>
+                            </label>
+                            <label className="Sign-UpDetails">
+                                Password
+                                <span className="SignUpcontainer">
+                                    <input type="password" name="password" id="password" placeholder="Enter a password..." />
+                                </span>
+                            </label>
+                            <button className="Sign-Up-Button" type="Create-User">Create Account</button>
+                        </div>
+                    </form>
+                </div>
+                </section>
+            </div>
+        )
+
+    }
+}
+
+export default SignUpPage
