53e59e3d5cd822974cdca7c572b57e5f9df7008e
mahowarth <87998897+mahowarth@users.noreply.github.com>
2021-10-11 23:30:01 +0100

    Fixed music playlist page

---
+++ src/searchDestination.js
@@ -0,0 +1,11 @@
+import './searchDestination.css';
+
+function SearchDestination() {
+    return(
+        <>
+            <h1>Where would you like to travel to today?</h1>
+        </>
+    );
+}
+
+export default SearchDestination;
